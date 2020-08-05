07b13d2cb92bbbf9e2fb3243d9e16e3fff676f22
Alisson Michel Sganzerla <alisson_sganzerla@yahoo.com.br>
2020-08-05 01:36:02 -0300

    netcheflix s/ thumbnail

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,6 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="og:image"
-          content="https://netcheflix.vercel.app/img/thumbnail.png"
-        />
         <meta name="description" content="Live Show do Guri" />
         <link
           rel="stylesheet"
